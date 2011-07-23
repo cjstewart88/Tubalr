153b3752310920a768fcf0dabed9f05cc43853f5
Cody Stewart <cjstewart88@gmail.com>
2011-07-23 14:46:48 -0400

    first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tubalr::Application.config.secret_token = '011c08a7915542c13f4645952be1469298f4ffef1c05481eb766bf6dea2022fb426d576106cd2f587e173070cd7176f1639c948b96cedd1bc2fc99dd14ff3f13'
