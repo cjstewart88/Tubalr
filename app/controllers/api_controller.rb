dbb01cfb8fc401f710c7587e5916f8746afe71e1
Cody Stewart <cjstewart88@gmail.com>
2011-08-10 13:50:44 -0400

    Changed the api calls to return an array.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,30 +1,30 @@
 class ApiController < ActionController::Base
   
   def just
-    results = {}
+    response = []
     
     youtubeData = HTTParty.get("http://gdata.youtube.com/feeds/api/videos?q=#{URI.escape(params[:artist_band])}&orderby=relevance&start-index=1&max-results=20&v=2&alt=json")
     
     youtubeData["feed"]["entry"].each_with_index do |(key,val), i|
-      results[i] = { :VideoID => key["id"]["$t"].split(":")[3], :VideoTitle => key["title"]["$t"] }
+      response.push(:VideoID => key["id"]["$t"].split(":")[3], :VideoTitle => key["title"]["$t"])
     end
     
-    render :json => results
+    render :json => response
   end
   
   def similar
-    results = {}
+    response = []
     
     lastfmData = HTTParty.get("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=#{URI.escape(params[:artist_band])}&limit=20&api_key=b25b959554ed76058ac220b7b2e0a026&format=json")
     
     lastfmData["similarartists"]["artist"].each_with_index do |(key,val), index|
       youtubeData = HTTParty.get("http://gdata.youtube.com/feeds/api/videos?q=#{URI.escape(key["name"])}&orderby=relevance&start-index=1&max-results=1&v=2&alt=json")
       youtubeData["feed"]["entry"].each do |key,val|
-        results[index] = { :VideoID => key["id"]["$t"].split(":")[3], :VideoTitle => key["title"]["$t"] }
+        response.push(:VideoID => key["id"]["$t"].split(":")[3], :VideoTitle => key["title"]["$t"])
       end
     end    
     
-    render :json => results
+    render :json => response
   end
   
 end
