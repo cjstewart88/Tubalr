6810d433370bc326e74df95c62df228fc363ea70
Cody Stewart <cjstewart88@gmail.com>
2011-11-22 15:30:27 -0500

    routes cleanup and a css id name change

--- config/routes.rb
+++ config/routes.rb
@@ -3,25 +3,25 @@ Tubalr::Application.routes.draw do
 
   root :to => "application#index"
   
-  get "/just/:artist_band.json" => "api#just"
-  get "/similar/:artist_band.json" => "api#similar"
-  get "/:username/favorites.json" => "api#userFavorites"
-  get "/:username/favorites/:search.json" => "api#userFavorites"
+  get "/just/:artist_band.json"               => "api#just"
+  get "/similar/:artist_band.json"            => "api#similar"
+  get "/:username/favorites.json"             => "api#userFavorites"
+  get "/:username/favorites/:search.json"     => "api#userFavorites"
   
-  get "/just/:artist_band" => "application#index"
-  get "/similar/:artist_band" => "application#index"
+  get "/just/:artist_band"                    => "application#index"
+  get "/similar/:artist_band"                 => "application#index"
   
-  post "/insert_search/:search_type/:search" => "application#search"
-  get "/stats" => "stats#recent_searches"
+  post "/insert_search/:search_type/:search"  => "application#search"
+  get "/stats"                                => "stats#recent_searches"
   
-  get "/:username/favorites" => "favorites#init"
-  get "/:username/favorites/:search" => "favorites#init"
+  get "/:username/favorites"                  => "favorites#init"
+  get "/:username/favorites/:search"          => "favorites#init"
   
-  post "/check-favorites" => "favorites#check"
-  post "/favorites/add" => "favorites#add"
-  post "/favorites/remove" => "favorites#remove"  
+  post "/check-favorites"                     => "favorites#check"
+  post "/favorites/add"                       => "favorites#add"
+  post "/favorites/remove"                    => "favorites#remove"  
   
   devise_scope :user do
-    get "/users/sign_out" => "devise/sessions#destroy"
+    get "/users/sign_out"                     => "devise/sessions#destroy"
   end
 end
