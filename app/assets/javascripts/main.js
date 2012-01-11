60f87987add706cbc41d9d462816c81a2bd91825
Cody Stewart <cjstewart88@gmail.com>
2012-01-11 16:12:58 -0500

    Still fucking with getting better search results. Still havnt fixed the issue with videos skipping :/

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -93,10 +93,10 @@ function just (who) {
   		else {
   		  $.each(data, function(i, tracks) {
     			var ajaxs = $.map(tracks.track, function(track) {
-    				return $.getJSON('http://gdata.youtube.com/feeds/api/videos?q='+escape(who)+'%20%2D$20'+escape(track.name)+'&restriction=US&orderby=relevance&start-index=1&max-results=1&v=2&alt=json-in-script&callback=?', function(data) {
+    				return $.getJSON('http://gdata.youtube.com/feeds/api/videos?q='+escape(who)+'%20%2D$20'+escape(track.name)+'&orderby=relevance&start-index=1&max-results=1&v=2&alt=json-in-script&callback=?', function(data) {
     					if (typeof data.feed.entry !== "undefined") {
     					  $.each(data.feed.entry, function(i,video) {
-    					    if (video.yt$accessControl[4].permission != "denied") {
+    					    if (video.yt$accessControl[4].permission != "denied" && video.media$group.media$category[0].$t == "Music" && unescape(who.toLowerCase().replace(/[ ]/g, "").replace(/[+]/g, "").replace(/["%20"]/g, "")) == video.title.$t.toLowerCase().split("-")[0].replace(/[ ]/g, "").replace(/[+]/g, "").replace(/["%20"]/g, "")) {
       					    var not_in_array = true;
       					    $.each(videos, function () {
       					      if (this.VideoID == video.id.$t.split(":")[3] || this.VideoTitle.toLowerCase().replace('/"'+who+'"/g', '').replace(/ *\([^)]*\) */g, "").replace(/ *\([^]]*\) */g, "").replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"") == video.title.$t.toLowerCase().replace('/"'+who+'"/g', '').replace(/ *\([^)]*\) */g, "").replace(/ *\([^]]*\) */g, "").replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"") || video.title.$t.toLowerCase().search("remix") != -1) {
@@ -177,7 +177,7 @@ function onYouTubePlayerAPIReady() {
     width: 498,
     height: 280,
     version: 3,
-    playerVars: { 'autoplay': 1, 'rel': 0, 'theme': 'dark', 'showinfo': 0, 'autohide': 1, 'wmode': 'opaque', 'allowScriptAccess': 'always', 'version': 3 },
+    playerVars: { 'autoplay': 1, 'rel': 0, 'theme': 'dark', 'showinfo': 0, 'autohide': 1, 'wmode': 'opaque', 'allowScriptAccess': 'always', 'version': 3, 'restriction': 'US' },
     events: {
       'onReady': onPlayerReady,
       'onStateChange': onPlayerStateChange,
