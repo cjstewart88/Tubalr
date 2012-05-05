a2570191679a3852b494bfba43b36f2d38e207f6
Cody Stewart <cjstewart88@gmail.com>
2012-05-05 19:28:11 -0400

    still playing with mobile shit

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -628,7 +628,7 @@ function detect_mobile () {
     $('.social').remove();
     $('#share').remove();
     $('#main').css('marginTop', 20);
-    $('nav').css({'textAlign': 'center', 'width': '400px', 'margin': '0 auto', 'position': 'relative', 'marginBottom': '15px', 'top': 5, 'fontSize': '12px'});
+    $('nav').css({'textAlign': 'center', 'width': '400px', 'margin': '0 auto', 'position': 'relative', 'marginBottom': '15px', 'top': 5, 'fontSize': '10px'});
     $('#about').text('Howdy mobile user, enter a band below and select just or similar!')
   }
 }
