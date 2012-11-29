e37d6b60316995acd53b135e8dbbf9e30d31d80e
jakswa <jakswa@gmail.com>
2012-11-28 19:31:26 -0500

    ugly firefox fix

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -4,6 +4,18 @@ var Player = {
   init: function () {
     var tag = document.createElement('script');
     tag.src = "http://www.youtube.com/player_api?version=3";
+
+    //incoming ugly fix for YT not firing onReady if the player element is hidden
+    if($.browser.mozilla) {
+      tag.onload = function() {
+        setTimeout(function() {
+          if (!$("#player").is(':visible')) {
+            $('#loading-playlist').hide();
+            $('#player').show(0, function(){$('#player').hide();});
+          }
+        }, 2000);
+      }
+    }
     var firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);    
   },
