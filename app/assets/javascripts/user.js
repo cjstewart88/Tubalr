2307f45de5d1af200579ccf50f4a81153e10ae54
Cody Stewart <cjstewart88@gmail.com>
2012-12-06 22:47:49 -0500

    basic username validation for new users and remove ability to edit username

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -8,6 +8,25 @@ var User = {
 
   init: function (options) {
     $.extend(User, options);
+  },
+
+  validateUsername: function () {
+    var usernameElement = $('#user_username');
+    var username        = usernameElement.val();
+
+    if (username.match(/[^a-z\d]/ig) != null || username == "") {
+      var error = $('<span>').addClass('error').text('only characters a-z & 0-9');
+      error.insertAfter(usernameElement.parent().find('label'));
+      return false;
+    }
   }
 
 };
+
+$(document).ready(function () {
+  
+  $('#user_new').submit(function () {
+    return User.validateUsername();
+  });
+
+});
