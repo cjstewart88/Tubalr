058e13c537d9e3569ebdbdb332763d2ae7178192
Cody Stewart <cjstewart88@gmail.com>
2013-01-05 22:13:25 -0500

    remove the rename migration and correct the original migration to add the md5_email column to the user table

--- db/migrate/20130106012201_add_md5_email_to_user.rb
+++ db/migrate/20130106012201_add_md5_email_to_user.rb
@@ -1,6 +1,6 @@
 class AddMd5EmailToUser < ActiveRecord::Migration
   def change
-    add_column :users, :md5_hash, :string
+    add_column :users, :md5_email, :string
 
   end
 end
--- db/migrate/20130106030221_raname_md5_hash_to_md5_email.rb
+++
@@ -1,8 +0,0 @@
-class RanameMd5HashToMd5Email < ActiveRecord::Migration
-  def self.up
-    rename_column :users, :md5_hash, :md5_email
-  end
-
-  def down
-  end
-end
