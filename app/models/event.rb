53cfcae1d0507849b7bddfeda40b3d89a75ca1a2
Cody Stewart <cjstewart88@gmail.com>
2013-01-08 17:57:02 -0500

    add default order to the event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ActiveRecord::Base
   belongs_to :user
+  default_scope order('created_at DESC')
 
   attr_accessible :event, :query, :playlist_name, :playlist_owner, :video_id, :video_title
 end
