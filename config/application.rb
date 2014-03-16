60b73b12476cd410fd741d8f52e101d620caa315
Cody Stewart <cjstewart88@gmail.com>
2014-03-15 22:51:17 -0400

    try to manualy expire assets by bumping the version

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Tubalr
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
+    config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
     config.assets.precompile += %w( *.css *.js )
     config.assets.enabled = true
